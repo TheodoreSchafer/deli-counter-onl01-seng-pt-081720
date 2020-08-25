5e2cd66755e73628bf9f7b8f9998d5eb694349af
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 00:05:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,24 @@ def line(array)
   case array.size
     when 0
       puts "The line is currently empty."
+    else
+      final = "The line is currently:"
+      array.each.with_index(1) do |customer, index|
+final += " #{index}. #{customer}"
+      end
+      puts final
     end
-
-
 end
 
-def take_a_number
-
-
+def take_a_number(array, name)
+ case array.size
+ when 0
+   welcome = "Welcome, "
+   array.each do |customer|
+  welcome
+    end
+     puts welcome
+   end
 end
 
 def now_serving(array)
