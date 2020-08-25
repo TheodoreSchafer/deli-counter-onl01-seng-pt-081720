44f82d2bf6ee789d9acbd1dcd8d3b1d459751880
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 00:39:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(array, name)
  case array.size
  when 0
    puts "Welcome, #{name}. You are number 1 in line."
-   welcome = "Welcome, "
+  
    array.each do |customer|
   welcome
     end
